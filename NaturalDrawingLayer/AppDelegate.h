ea3febd51bba6c5427ca5cfb1582c89a8c7f236f
Randy McLain <randy.mclain@ikegps.com>
2017-02-02 13:08:25 -0800

    Initial Commit

---
+++ NaturalDrawingLayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NaturalDrawingLayer
+//
+//  Created by Randy McLain on 2/2/17.
+//  Copyright © 2017 Randy McLain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NaturalDrawingLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NaturalDrawingLayer
+//
+//  Created by Randy McLain on 2/2/17.
+//  Copyright © 2017 Randy McLain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
